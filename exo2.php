c438673fdd588bde3cee426ebf942adb687f688a
julie-devos <julie.devos@epitech.eu>
2020-12-07 18:17:00 +0100

    initial commit

---
+++ exo2.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Exercice 2-Partie 2</title>
+</head>
+<body>
+
+<?php $isEasy=true; ?>
+<?=$isEasy==true ? "C'est facile !!" : "C'est difficile !!!";?><br>
+
+<?php $isEasy=false; ?>
+<?=$isEasy==0 ? "C'est difficile !!!" : "C'est facile !!";?><br>
+
+</body>
+</html>
